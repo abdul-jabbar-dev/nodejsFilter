fa50e2db13cbfff2ae21a785b51c27da172466fc
abduljabbar15 <abduljabber1532002@gmail.com>
2022-09-19 18:51:21 +0600

    w]

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { default: mongoose } = require('mongoose')
 const tourRouter = require('./Routers/tour.router')
 const app = express()
+const port = process.env.PORT||4000
 mongoose.connect(`mongodb+srv://abdul:abduljabbar@cluster0.cle1zkm.mongodb.net/plans?retryWrites=true&w=majority`, (err) => console.log('db is connected'))
 
 app.use(express.json())
@@ -9,4 +10,4 @@ app.use(express.json())
 app.use('/tours', tourRouter)
 app.use('/', (req, res) => res.send('home'))
 
-app.listen(4000, (res) => console.log('port is running'))
+app.listen(port, (res) => console.log('port is running'))
